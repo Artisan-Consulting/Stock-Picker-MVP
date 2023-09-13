959d940bafbe43b583783666b48c3cda50364434
William Kwan <william.kwan@artisanconsulting.nz>
2023-09-13 14:12:05 +1000

    resync

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// Stock Picker:  Find Button
 
+document.getElementById('btnFind').addEventListener('click', findStock);
+
+//document.querySelector('btnFind').addEventListener('click', findStock);
+
+function findStock() {
+    console.log ("even triggered")
+    alert ("triggered")
+}
+
+
+
+// button.addEventListener('click', () => {
+//     console.log('Button clicked EventListener')
+// })
+
+// function addClickEvent(target){
+//     target.addEventListener('click', (e)=>{
+//         window.alert ("Clicked")
+//         console.log ("clicked")
+//     })
+
+// }
