9da05d8382bd2d7bb3cfe1e52aa5009b74026cd3
William Kwan <william.kwan@artisanconsulting.nz>
2023-09-26 08:07:19 -0400

    .env created

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 // Stock Picker:  Find Button
 require('dotenv').config();
-
-//console.log(process.env);
-
-const api_key = process.env.FINNHUB_API_KEY;
-
+console.log (process.env.FINNHUB_API_KEY);
 
 document.getElementById('btnFind').addEventListener('click', findStock);
 
-//document.querySelector('btnFind').addEventListener('click', findStock);
-
 function findStock() {
-    console.log ("even triggered")
-    alert ("triggered")
+   
+  
+    //console.log(process.env);
+    const api_key = process.env.FINNHUB_API_KEY;
+    alert ("Clicked")
+    //alert(process.env.FINNHUB_API_KEY)
+
+    // const url = "https://finnhub.io/api/v1/stock/symbol?exchange=US&token=";
+    // const finnhubUrl = url+api_key
+ 
+    // console.log ("even triggered")
+    // alert ("triggered")
     //document.querySelector("section#stock-picker-header h1').innerHTML = "$244.88"
-    //
+
 }
 
 
